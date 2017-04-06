6acbdadf55246024b767d49fd2cf56e7d694f84d
RuiCheung <zhang1rui4@foxmail.com>
2017-04-06 10:17:45 +0800

    添加字典读取函数

--- util/dictutil.py
+++ util/dictutil.py
@@ -2,13 +2,13 @@
 import json
 import codecs
 
-def buildDict(file_path,
-              dict_path,
-              vocab_size=30000,
-              encoding='utf8'):
+def build_dict(file_name,
+               dict_path,
+               vocab_size=30000,
+               encoding='utf8'):
     dictionary = {}
 
-    file_in = json.load(open(file_path), encoding)
+    file_in = json.load(open(file_name), encoding)
 
     for line in file_in:
         question, answer = line
@@ -39,10 +39,12 @@ def buildDict(file_path,
     # 将排序后的词典进行输出
     '''
     with codecs.open(dict_path, 'w', encoding) as f_out:
-        f_out.write('<unk>')
-        f_out.write('\t')
+        f_out.write('<unk>\t')
         f_out.write(str(unk_count))
         f_out.write('\n')
+        f_out.write('<pad>\t0\n')
+        f_out.write('<start>\t0\n')
+        f_out.write('<end>\t0\n')
 
         for word, freq in sorted_dict:
             f_out.write(word)
@@ -52,24 +54,38 @@ def buildDict(file_path,
     '''
 
     # 根据出现频率设置阈值,低于阈值的采用<unk>代替
-    threhold = 30000
-
     unk_count = 0
-    for _, freq in sorted_dict[(threhold-1):]:
+    for _, freq in sorted_dict[(vocab_size-4):]:
         unk_count += freq
 
-    dict_name = dict_path+'dict_' + str(threhold) + '.dict'
+    dict_name = dict_path+'dict_' + str(vocab_size) + '.dict'
 
     with codecs.open(dict_name, 'w', encoding) as f_out:
-        f_out.write('<unk>')
-        f_out.write('\t')
+        f_out.write('<unk>\t')
         f_out.write(str(unk_count))
         f_out.write('\n')
+        f_out.write('<pad>\t0\n')
+        f_out.write('<start>\t0\n')
+        f_out.write('<end>\t0\n')
 
-        for word, freq in sorted_dict[:(threhold-1)]:
+        for word, freq in sorted_dict[:(vocab_size-4)]:
             f_out.write(word)
             f_out.write('\t')
             f_out.write(str(freq))
             f_out.write('\n')
 
 
+def load_dict(dict_name,
+              encoding='utf8'):
+    word_to_idx = {}
+    idx_to_word = {}
+
+    with codecs.open(dict_name, 'r', encoding) as f_in:
+        for line in f_in:
+            word = line.strip().split("\t")[0]
+            word_to_idx[word] = len(word_to_idx)
+            idx_to_word[len(idx_to_word)] = word
+
+    return word_to_idx, idx_to_word
+
+build_dict("./../data/train_data.json", './../dict/')
