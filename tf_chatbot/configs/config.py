20838b88ff7162060b0a90260cd248c83c69fe31
RuiZhang1993 <zhang1rui4@foxmail.com>
2017-05-17 21:08:50 +0800

    commit

--- tf_chatbot/configs/config.py
+++ tf_chatbot/configs/config.py
@@ -10,8 +10,8 @@ tf.app.flags.DEFINE_string('results_dir', SAVE_DATA_DIR+'results', 'Train direct
 tf.app.flags.DEFINE_float('learning_rate', 0.5, 'Learning rate')
 tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.99, 'Learning rate decays by this much.')
 tf.app.flags.DEFINE_float('max_gradient_norm', 5.0, 'Clip gradients to this norm')
-tf.app.flags.DEFINE_integer('batch_size', 256, 'Batch size to use during training')
-tf.app.flags.DEFINE_integer('epoch_size', 20, 'Size of epoch')
+tf.app.flags.DEFINE_integer('batch_size', 128, 'Batch size to use during training')
+tf.app.flags.DEFINE_integer('epoch_size', 3, 'Size of epoch')
 
 tf.app.flags.DEFINE_integer('vocab_size', 20000, 'Dialog vocabulary size')
 tf.app.flags.DEFINE_integer('size', 128, 'size of each model layer')
