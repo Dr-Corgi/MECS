3d4481ebff7cb578bb63c061d45705679618f00a
RuiZhang1993 <zhang1rui4@foxmail.com>
2017-05-17 13:50:26 +0800

    fix bug

--- tf_chatbot/configs/config.py
+++ tf_chatbot/configs/config.py
@@ -10,7 +10,7 @@ tf.app.flags.DEFINE_string('results_dir', SAVE_DATA_DIR+'results', 'Train direct
 tf.app.flags.DEFINE_float('learning_rate', 0.5, 'Learning rate')
 tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.99, 'Learning rate decays by this much.')
 tf.app.flags.DEFINE_float('max_gradient_norm', 5.0, 'Clip gradients to this norm')
-tf.app.flags.DEFINE_integer('batch_size', 128, 'Batch size to use during training')
+tf.app.flags.DEFINE_integer('batch_size', 256, 'Batch size to use during training')
 tf.app.flags.DEFINE_integer('epoch_size', 20, 'Size of epoch')
 
 tf.app.flags.DEFINE_integer('vocab_size', 20000, 'Dialog vocabulary size')
@@ -18,7 +18,7 @@ tf.app.flags.DEFINE_integer('size', 128, 'size of each model layer')
 tf.app.flags.DEFINE_integer('num_layers', 1, 'Numbers of layers in the model')
 
 tf.app.flags.DEFINE_integer('max_train_data_size', 0, 'Limit on the size of training data (0: no limit)')
-tf.app.flags.DEFINE_integer('steps_per_checkpoint', 100, 'How many training steps to do per checkpoint')
+tf.app.flags.DEFINE_integer('steps_per_checkpoint', 500, 'How many training steps to do per checkpoint')
 
 FLAGS = tf.app.flags.FLAGS
 
